d64fc02806860cab1caea5769ac892a55dcf4c35
= <mpenaloza@uc.cl>
2023-08-19 01:34:46 -0400

    feat: First elements of mobile app

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
+import Loading from './components/Loading';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
-  return (
+  const [fetchingData, setFetchingData] = useState(true);
+  const [movieData, setMovieData] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: 'https://moviesdatabase.p.rapidapi.com/titles/random',
+    params: {
+      list: 'top_rated_english_250',
+      limit: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '52b235e227msh0fe034ed9911bf2p15178ejsndd0809228858',
+      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    }
+  };
+
+  async function getRandomMovie() {
+    try {
+      const response = await axios.request(options);
+      setMovieData(response.data.results[0]);
+      setFetchingData(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getRandomMovie();
+  }, [])
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {fetchingData ? <Loading /> : <Home movie={movieData} getRandomMovie={getRandomMovie} />}
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
---
+++ components/Loading.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+  return(
+    <View style={styles.container}>
+      <Text>Loading...</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
