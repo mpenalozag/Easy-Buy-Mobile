19ec1344202428f5e33150c9b2b48c7052905ae4
= <mpenaloza@uc.cl>
2023-08-22 00:16:07 -0400

    edit: refactor of code

--- app/MainContainer.jsx
+++ app/MainContainer.jsx
@@ -1,39 +1,40 @@
 import { View, Text, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {Feather} from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 import Home from "./Home";
 import Cart from "./Cart";
 import Scanner from "./Scanner";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Tab = createBottomTabNavigator();
 
 export default function MainContainer() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Inicio') {
-              iconName = focused ? 'home' : 'home';
-            } else if (route.name === 'Carro') {
-              iconName = focused ? 'shopping-cart' : 'shopping-cart';
-            } else if (route.name === 'Escanear') {
-              iconName = focused ? 'camera' : 'camera';
-            }
-            return <Feather name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#2196F3',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name={"Inicio"} component={Home} />
-        <Tab.Screen name={"Escanear"} component={Scanner} />
-        <Tab.Screen name={"Carro"} component={Cart} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Inicio') {
+            iconName = focused ? 'home' : 'home';
+          } else if (route.name === 'Carro') {
+            iconName = focused ? 'shopping-cart' : 'shopping-cart';
+          } else if (route.name === 'Escanear') {
+            iconName = focused ? 'camera' : 'camera';
+          }
+          return <Feather name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#2196F3',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabel: () => {
+          return null;
+        },
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name={"Inicio"} component={Home} />
+      <Tab.Screen name={"Escanear"} component={Scanner} />
+      <Tab.Screen name={"Carro"} component={Cart} />
+    </Tab.Navigator>
   )
 }
