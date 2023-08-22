86a69d1ffc0b6a856af5c10fb1f4dd552c41f833
= <mpenaloza@uc.cl>
2023-08-21 22:41:09 -0400

    feat: Everything removed and Main Container is the only remainder

--- App.jsx
+++ App.jsx
@@ -2,32 +2,12 @@ import { StyleSheet, View, Text, Button, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Constants from 'expo-constants';
 
+import MainContainer from "./app/MainContainer";
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {
-        <View style={styles.container}>
-          <View style={styles.elements}>
-            <View style={styles.texts}>
-              <Text style={styles.title}>EasyCart</Text>
-              <Text>Instántaneo, fácil, seguro</Text>
-            </View>
-            <View style={styles.buttons}>
-              <Button
-                title={'Escanear producto'}
-                onPress={() => alert("Escanear producto")}
-              />
-              <Button
-                title={'Ver carro'}
-                onPress={() => alert("Ver carro")}
-              />
-            </View>
-          </View>
-        </View>
-      }
-    </NavigationContainer>
+    <MainContainer />
   )
 }
 
